ec9baabae90b6f7bd4ad714c8cf8ee527207cd82
Imanhgz <46437967+Gartleypattern@users.noreply.github.com>
2023-11-22 19:04:04 -0500

    Add imageFilenames and imageAlt constants in main.js

--- Assignment 4- JavaScript/Image gallery/main.js
+++ Assignment 4- JavaScript/Image gallery/main.js
@@ -1,18 +1,32 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
 
 /* Declaring the array of image filenames */
+const imageFilenames = [
+  "images/pic1.jpg",
+  "images/pic2.jpg",
+  "images/pic3.jpg",
+  "images/pic4.jpg",
+  "images/pic5.jpg",
+];
 
 /* Declaring the alternative text for each image file */
+const imageAlt = [
+  "Closeup of a human eye",
+  "ُSee waves",
+  "Flowers",
+  "Ancient painting",
+  "Butterfly",
+];
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
+const newImage = document.createElement("img");
+newImage.setAttribute("src", xxx);
+newImage.setAttribute("alt", xxx);
 thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
